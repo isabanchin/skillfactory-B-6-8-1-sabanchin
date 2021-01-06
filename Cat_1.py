50168d82858e2ba97f867e2e7ab7ca2e5a61a53f
isabanchin <72926280+isabanchin@users.noreply.github.com>
2021-01-06 18:13:48 +0800

    Add files via upload

---
+++ Cat_1.py
@@ -0,0 +1,5 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.n = name
+        self.g = gender
+        self.a = age
---
+++ Cat_2.py
@@ -0,0 +1,6 @@
+from Cat_1 import Cat
+c1 = Cat('Барон', 'мальчик', 2)
+c2 = Cat('Сэм', 'мальчик', 3)
+print(c1.n + " " + c1.g + " " + str(c1.a))
+print(c2.n + " " + c2.g + " " + str(c2.a))
+
